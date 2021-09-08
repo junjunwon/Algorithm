8a232d623c6791c3a9461c326f7bef7bdbc6a583
junjunwon <wnsgh1993@gmail.com>
2021-09-09 08:43:40 +0900

    double linkedList에 addNode, printAll 메소드 추가

--- LinkedList/DoublyLinkedList.java
+++ LinkedList/DoublyLinkedList.java
@@ -1,5 +1,6 @@
 package LinkedList;
 
+
 public class DoublyLinkedList<T> {
     /**
      * 
@@ -19,8 +20,43 @@ public class DoublyLinkedList<T> {
             this.data = data;
         }
     }
+
+    public void addNode(T data){
+        if(this.head == null){
+            Node<T> node = new Node<T>(data);
+            this.head = node;
+            this.tail = head;
+        } else{
+            Node<T> node = this.head;
+            while(node.next != null){
+                node = node.next;
+            }
+            node.next = new Node<T>(data);
+            node.next.prev = node;
+            this.tail = node.next;
+        }
+    }
+
+    public void printAll(){
+        if(this.head!=null){
+            Node<T> node = this.head;
+            System.out.println(node.data);
+
+            while(node.next != null){
+                node=node.next;
+                System.out.println(node.data);
+
+            }
+        }
+    }
     
     public static void main(String[] args){
-        
+        DoublyLinkedList<Integer> linkedList = new DoublyLinkedList<>();
+
+        linkedList.addNode(1);
+        linkedList.addNode(2);
+        linkedList.addNode(3);
+        linkedList.printAll();
+
     }
 }
